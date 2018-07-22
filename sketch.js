7b02b7d3aec7eb57b9221135f385d725a42c9580
prabal dev <35391821+devprabal@users.noreply.github.com>
2018-07-22 15:21:41 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,23 +1,44 @@
 var capture;
+var heart;
+var pd;
+
+function preload() {
+  heart = loadModel('Heart.obj');
+}
 
 function setup() {
-  createCanvas(480, 480, WEBGL);
+  createCanvas(windowWidth, windowHeight, WEBGL);
   capture = createCapture(VIDEO);
   capture.hide();
   // cam.size(200,200);
+
 }
 var angle = 0;
+
 function draw() {
   // image(capture, 0, 0, width, width * capture.height / capture.width);
   // filter(INVERT);
   background(200);
+
   noStroke();
+  push();
   texture(capture);
   rectMode(CENTER);
   rotateX(angle);
   rotateY(angle * 1.2);
   rotateZ(angle * 0.02);
-  // fill(200, 100, 200);
   box(200);
+  pop();
+
+  push();
+  translate(-150, -50, 0);
+  noStroke();
+  rotateX(angle);
+  rotateY(angle * 0.2);
+  rotateZ(angle * 0.2);
+  normalMaterial();
+  model(heart);
+  pop();
   angle += 0.01;
+
 }
